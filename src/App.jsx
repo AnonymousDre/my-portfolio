e76e625f9388b8722e73c4253d6553e599ac5ce4
Andre Quizon <135997258+AnonymousDre@users.noreply.github.com>
2026-03-26 21:01:57 +0800

    Update App.jsx
    catered with component calling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import Sidebar from "./Sidebar";
 import Landing from "./Landing";
+import About from "./About";
+import Services from "./Services";
+import Projects from "./Projects";
+import Awards from "./Awards";
+import FAQs from "./FAQs";
 
 export default function Portfolio() {
   return (
     <div className="app-layout">
       <Sidebar />
       <main className="app-content">
-        <Landing />
+        <div className="landing">
+          <Landing />
+          <About />
+          <Services />
+          <Projects />
+          <Awards />
+          <FAQs />
+        </div>
       </main>
     </div>
   );
