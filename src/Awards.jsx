94d301a1c5712982be99833c62f5248fa296b2f2
Andre Quizon <135997258+AnonymousDre@users.noreply.github.com>
2026-03-28 15:43:02 +0800

    Update Awards.jsx
    replaced placeholder text

--- src/Awards.jsx
+++ src/Awards.jsx
@@ -2,7 +2,8 @@ export default function Awards() {
   return (
     <section id="awards" className="landing-section" aria-label="Awards">
       <h1 className="title">Awards</h1>
-      <div className="placeholder">Placeholder content</div>
+      <div className="placeholder">Dean’s Lister for 7 Consecutive Semesters</div>
+      
     </section>
   );
 }
