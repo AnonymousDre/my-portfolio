5241594a6aebaffacf653329af4e603cb116e9f7
Andre Quizon <135997258+AnonymousDre@users.noreply.github.com>
2026-03-28 15:43:31 +0800

    Update Services.jsx
    placeholder services

--- src/Services.jsx
+++ src/Services.jsx
@@ -2,7 +2,18 @@ export default function Services() {
   return (
     <section id="services" className="landing-section" aria-label="Services">
       <h1 className="title">Services</h1>
-      <div className="placeholder">Placeholder content</div>
+      <div className="services-box">
+        <div className="services-box-item">
+          <h2 className="subtitle">Custom Web, E-Commerce Solutions</h2>
+          <p></p>
+        </div>
+
+        <div className="services-box-item">
+          <h2 className="subtitle">Available Across All Time Zones</h2>
+          <p></p>
+        </div>
+
+      </div>
     </section>
   );
 }
